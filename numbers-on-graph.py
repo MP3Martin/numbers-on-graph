b8a9a21914b68029831a879ca419ebbb2788bb16
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2023-12-06 10:41:28 +0100

    Add Collatz conjecture, remove some repeating code

--- numbers-on-graph.py
+++ numbers-on-graph.py
@@ -1,5 +1,6 @@
 # By MP3Martin @ https://github.com/MP3Martin/numbers-on-graph
 # MIT licensed
+# Too lazy to rewrite this code
 
 import matplotlib.pyplot as plt
 import input_num
@@ -9,47 +10,37 @@ import mplcursors
 import sys
 from platform import system
 
-#from SO
-def plt_maximize():
-    # See discussion: https://stackoverflow.com/questions/12439588/how-to-maximize-a-plt-show-window-using-python
-    backend = plt.get_backend()
-    cfm = plt.get_current_fig_manager()
-    if backend == "wxAgg":
-        cfm.frame.Maximize(True)
-    elif backend == "TkAgg":
-        if system() == "Windows":
-            cfm.window.state("zoomed")  # This is windows only
-        else:
-            cfm.resize(*cfm.window.maxsize())
-    elif backend == "QT4Agg":
-        cfm.window.showMaximized()
-    elif callable(getattr(cfm, "full_screen_toggle", None)):
-        if not getattr(cfm, "flag_is_max", None):
-            cfm.full_screen_toggle()
-            cfm.flag_is_max = True
-    else:
-        raise RuntimeError("plt_maximize() is not implemented for current backend:", backend)
-
 args_list = list(sys.argv)[1:]
-args = {"mode" : "", "ncount" : ""}
+args = {"mode" : "", "ncount" : "", "data": ""}
 
-try:
-  args_list[0]
-except:
-  pass
-else:
-  args["mode"] = args_list[0]
+def getData(text, data_type = str):
+  if not args["data"]:
+    if data_type == int:
+      data_input = int(input_num(text, True, False))
+    else:
+      data_input = input(text)
+    print("")
+  else:
+    data_input = args["data"]
+  try:
+    return data_type(data_input)
+  except:
+    print(f"Error: Invalid type for the \"data\" arg (got {type(data_input).__name__}, expected {data_type.__name__})!")
+    exit()
+  
+def setPltTitle(text):
+  plt.title(text, fontsize=18)
 
-try:
-  args_list[1]
-except:
-  pass
-else:
-  args["ncount"] = args_list[1]
+for key in args:
+  i = list(args.keys()).index(key)
+  try:
+    args_list[i]
+  except:
+    pass
+  else:
+    args[key] = args_list[i]
 
-# print(args)
-
-MODES = ["Fibonacci", "Pi", "Recamán's Sequence"]
+MODES = ["Fibonacci", "Pi", "Recamán's Sequence", "Collatz conjecture"]
 
 plot_y = []
 plot_x = []
@@ -64,16 +55,16 @@ mode = -1
 def getMode():
   global mode
   if not args["mode"]:
-    mm = int(input_num("Mode: ", True, False))
+    mode_input = int(input_num("Mode: ", True, False))
   else:
     try:
-      mm = int(args["mode"])
+      mode_input = int(args["mode"])
     except:
       if not args["mode"]:
         print("")
       print("Error: Mode must be integer!")
       exit()
-  if mm > len(MODES) - 1 or mm < 0:
+  if mode_input > len(MODES) - 1 or mode_input < 0:
     if not args["mode"]:
         print("")
     print("Error: Invalid mode!")
@@ -81,14 +72,11 @@ def getMode():
       exit()
     getMode()
   else:
-    mode = mm
+    mode = mode_input
     if not args["mode"]:
       print("")
 
 getMode()
-# print(mode)
-
-# print("")
 
 if not args["ncount"]:
   COUNT = int(input_num("Number count: ", True, False))
@@ -101,11 +89,12 @@ else:
     exit()
 
 if COUNT < 2:
-    print("Error: Minimally 2 numbers are required!")
-    exit()
-else:
-  pass
+  print("Error: Minimally 2 numbers are required!")
+  exit()
 count = int(COUNT)
+if mode == 3:
+  start = getData("Start from: ", int)
+  
 print("Loading...\n")
 
 if mode == 0:
@@ -113,14 +102,14 @@ if mode == 0:
   global fib
   fib = [0, 1]; # init array
   count = count + i
-  plt.title('Fibonacci numbers on graph', fontsize=18)
+  setPltTitle('Fibonacci numbers on graph')
   for i in range(i, count):
     # print(fib[i - 2])
 
     fib.insert(i, fib[i - 2] + fib[i - 1])
     plot_y.append(fib[i])
 elif mode == 1:
-  plt.title('Pi numbers on graph', fontsize=18)
+  setPltTitle('Pi numbers on graph')
 
   DIGITS = count
 
@@ -140,26 +129,25 @@ elif mode == 1:
 
   plot_y = list(digits)
 
-  # plot_y.append(99)
-
-  # if len(plot_y) >= count:
-  #   plot_y = plot_y[0:count]
-  #   # print(plot_y)
-  # else:
-  #   print("Error: Not enough numbers to plot!")
-  #   exit()
 elif mode == 2:
-  plt.title('Recamán\'s sequence on graph', fontsize=18)
+  setPltTitle('Recamán\'s sequence on graph')
   R = [0]; [(R:= R + [R[-1]+x]) if R[-1]-x in R or R[-1] < x else (R := R + [R[-1]-x]) for x in range(1,count)]
   plot_y = list(R)
+  
+elif mode == 3:
+  setPltTitle('Collatz conjecture on graph')
+  digits = [start]
+  for i in range(count):
+    if digits[-1] % 2 == 0:
+      digits.append(digits[-1] / 2)
+    else:
+      digits.append((digits[-1] * 3) + 1)
+  plot_y = digits
 
 
-
+ 
 plot_x = list(range(len(plot_y)))
-# plot_x = list(range(10))
 
-# print(plot_y)
-# print(plot_x)
 plt.plot(plot_x, plot_y)
   
 plt.xlabel('x', fontsize=18)
@@ -167,11 +155,6 @@ plt.ylabel('y', fontsize=18)
 
 # function to show the plot
 mng = plt.get_current_fig_manager()
-mng.resize(*mng.window.maxsize())
-try:
-  plt_maximize()
-except:
-  pass
 plt.get_current_fig_manager().set_window_title('Matplotlib - Python')
 
 print("Loaded!")
@@ -179,12 +162,8 @@ print("Loaded!")
 def show_datapoints(sel):
   xi, yi = sel[0], sel[0]
   xi, yi = xi._xorig.tolist(), yi._yorig.tolist()
-  # print(xi[int(round(sel.target.index))])
-  # exit()
   sel.annotation.set_text('y: '+ str(yi[int(round(sel.index))])) # +'\n'+ 'y: '+ str(yi[int(round(sel.target.index))])
 
 mplcursors.cursor(hover=True).connect('add',show_datapoints)
-
-# print(plot_y)
 
 plt.show()
